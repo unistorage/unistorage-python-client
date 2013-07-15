8c6c18cd91432c957bf09b63c3c530f10550719e
Anton Romanovich <anthony.romanovich@gmail.com>
2013-07-15 17:17:00 +0600

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='unistorage',
-    version='0.0.1',
+    version='0.0.2',
     description='Python client for the Unistorage API',
     url='https://github.com/unistorage/unistorage-python-client',
 
